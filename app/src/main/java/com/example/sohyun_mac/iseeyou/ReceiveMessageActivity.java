7d5e8cf4c2596c21f351294c4714f4082757f67e
SohyunDev <mohazzang@gmail.com>
2017-11-29 17:30:45 +0900

    add NCF Receive, send Activity

--- app/src/main/java/com/example/sohyun_mac/iseeyou/ReceiveMessageActivity.java
+++ app/src/main/java/com/example/sohyun_mac/iseeyou/ReceiveMessageActivity.java
@@ -1,13 +1,139 @@
 package com.example.sohyun_mac.iseeyou;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+
+    private TextView readText;
+    private Button btn_receive;
+    private NfcAdapter mNfcAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+    private Tag tag;
+    private IsoDep tagcomm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        readText = (TextView)findViewById(R.id.readText);
+        btn_receive = (Button)findViewById(R.id.btn_receive);
+        btn_receive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readNFC();
+            }
+        });
+    }
+    public void readNFC(){
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if(mNfcAdapter==null){
+            Toast.makeText(this,"NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        else{
+            Toast.makeText(this,"NFC is avilable", Toast.LENGTH_LONG).show();
+        }
+
+        Intent intent = new Intent(this,getClass());
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        mPendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try{
+            ndef.addDataType("*/*");
+            mFilters = new IntentFilter[]{ndef};
+        }
+        catch(Exception e){
+            throw new RuntimeException("fail", e);
+        }
+        mTechLists = new String[][]{new String[]{NfcA.class.getName()}};
+    }
+
+    /*
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(mNfcAdapter != null){
+            mNfcAdapter.enableForegroundDispatch(this, mPendingIntent,mFilters, mTechLists);
+        }
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())){
+            onNewIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if(mNfcAdapter != null){
+            mNfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+    */
+
+    @Override
+    public void onNewIntent(Intent intent)
+    {
+        String action = intent.getAction();
+        String tag = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_TAG).toString();
+        String strMsg = action + "\n\n"+tag;
+        readText.setText(strMsg);
+
+        Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if(messages == null){
+            return;
+        }
+        for(int i=0; i<messages.length; i++){
+            showMsg((NdefMessage)messages[i]);
+        }
+    }
+
+    public void showMsg(NdefMessage mMessage){
+        String strMsg = "", strRec="";
+        NdefRecord[] recs = mMessage.getRecords();
+        for(int i=0; i<recs.length; i++){
+            NdefRecord record = recs[i];
+            byte[] payload = record.getPayload();
+            if(Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)){
+                strRec = byteDecoding(payload);
+                strRec = "Text: " + strRec;
+            }
+            //TODO :  RTD_TEXT가 아닐 경우 (Send에서 RTD_TEXT)만 해서 상관없을듯
+
+            strMsg+=("\n\nNdefRecord["+i+"]:\n"+strRec);
+        }
+        readText.append(strMsg);
+    }
+
+    public String byteDecoding(byte[] buf){
+        String strTxt="";
+        String textEncoding=((buf[0]&0200)==0)?"UTF-8":"UTF-16";
+        int langCodeLen = buf[0]&0077;
+
+        try{
+            strTxt=new String(buf, langCodeLen+1, buf.length-langCodeLen-1,textEncoding);
+        }
+        catch (Exception e){
+            Log.d("tag1",e.toString());
+        }
+        return strTxt;
     }
 }
--- app/src/main/java/com/example/sohyun_mac/iseeyou/SendMessageActivity.java
+++ app/src/main/java/com/example/sohyun_mac/iseeyou/SendMessageActivity.java
@@ -1,13 +1,115 @@
 package com.example.sohyun_mac.iseeyou;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SendMessageActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+public class SendMessageActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback {
+
+    NfcAdapter mNfcAdapter;
+    String sendMessageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        Button btn_send = (Button)findViewById(R.id.btn_send);
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readSMSMessage();
+                mNfcAdapter = NfcAdapter.getDefaultAdapter(SendMessageActivity.this);
+                if(mNfcAdapter==null){
+                    Toast.makeText(SendMessageActivity.this,"NFC is not available", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                else{
+                    Toast.makeText(SendMessageActivity.this,"NFC is avilable", Toast.LENGTH_LONG).show();
+                }
+                mNfcAdapter.setNdefPushMessageCallback(SendMessageActivity.this, SendMessageActivity.this);
+            }
+        });
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        return new NdefMessage(new NdefRecord[]{createMimeRecord(sendMessageText, Locale.KOREAN, true)});
+    }
+
+    public NdefRecord createMimeRecord(String text, Locale locale, boolean encodeInUtf8){
+        final byte[] langBytes = locale.getLanguage().getBytes(StandardCharsets.US_ASCII);
+        final Charset utfEncoding = encodeInUtf8 ? StandardCharsets.UTF_8 : Charset.forName("UTF-16");
+        final byte[] textBytes = text.getBytes(utfEncoding);
+        final int utfBit = encodeInUtf8 ? 0 : (1<<7);
+        final char status = (char)(utfBit+langBytes.length);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            outputStream.write(new byte[]{(byte)status});
+            outputStream.write(langBytes);
+            outputStream.write(textBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final byte[] data = outputStream.toByteArray();
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
+    }
+
+    public void readSMSMessage(){
+        Uri allMesseage = Uri.parse("content://sms");
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(allMesseage, new String[]{"_id","thread_id","address","person","date","body"},
+                null, null, "date DESC");
+
+        if(cursor != null) {
+            MessageItem messageItem = new MessageItem();
+
+            long messageId = cursor.getLong(0);
+            messageItem.setMessageId(String.valueOf(messageId));
+
+            long threadId = cursor.getLong(1);
+            messageItem.setThreadId(String.valueOf(threadId));
+
+            String phoneNum = cursor.getString(2);
+            messageItem.setPhoneNum(phoneNum);
+
+            long contactId = cursor.getLong(3);
+            messageItem.setContactId(String.valueOf(contactId));
+
+            String contactId_string = String.valueOf(contactId);
+            messageItem.setContactId_string(contactId_string);
+
+            long time = cursor.getLong(4);
+            messageItem.setTime(String.valueOf(time));
+
+            String body = cursor.getString(5);
+            messageItem.setBody(body);
+
+            cursor.close();
+
+            sendMessageText = "";
+            String newLine = "\n";
+            String phoneNumText = "전화번호 : ";
+            String timeText = "시간 : ";
+            String bodyText = "내용 : ";
+            sendMessageText += phoneNumText + messageItem.getPhoneNum() + newLine;
+            sendMessageText += timeText + messageItem.getTime() + newLine;
+            sendMessageText += bodyText + messageItem.getBody() + newLine;
+        }
     }
 }
